45682de95401c0b9c34e1a308c4dcac6e0e6201c
Islam Gad <islam.gad@elsewedy.com>
2023-07-05 11:10:54 +0300

    Style orders table with bootstrap

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -10,17 +10,17 @@ const Dashboard = () => {
         {
             id: 1, date: new Date(), products:
                 [
-                    {  id: 1,productName: 'Rice', Price: 5, quantity: 1 },
-                    {  id: 2,productName: 'Banana', Price: 10, quantity: 2 },
-                    {  id: 3,productName: 'Shoes', Price: 5, quantity: 1 }
+                    { id: 1, productName: 'Rice', Price: 5, quantity: 1 },
+                    { id: 2, productName: 'Banana', Price: 10, quantity: 2 },
+                    { id: 3, productName: 'Shoes', Price: 5, quantity: 1 }
                 ]
         },
 
         {
             id: 2, date: new Date(), products:
                 [
-                    {  id: 4,productName: 'Rice', Price: 7.5, quantity: 1 },
-                    {  id: 5,productName: 'Tomoto', Price: 10, quantity: 2 },
+                    { id: 4, productName: 'Rice', Price: 7.5, quantity: 1 },
+                    { id: 5, productName: 'Tomoto', Price: 10, quantity: 2 },
                 ]
         }
 
@@ -29,40 +29,34 @@ const Dashboard = () => {
     return (
         <div className='dashboard'>
             <h2>Order Management Dashboard</h2>
-            <p>Welcome {params.userName} </p>
-
-            <br></br><hr></hr><br></br>
-
-            {
-                orders.map(
-                    order => (
-                        <div key={order.id}>
-                            <h2>Order Id {order.id}, {order.date.toString()}</h2>
-                            {order.products.map(
-                                product => (
-                                    <table className="table" key={`${order.id}-${product.id}`}>
-                                        <thead>
-                                            <tr>
-                                                <th>Product Name</th>
-                                                <th>Price</th>
-                                                <th>quantity</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>{product.productName}</td>
-                                                <td>{product.price}</td>
-                                                <td>{product.quantity}</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                )
-                            )}
-                        </div>
-                    )
+            <p>Welcome {params.userName}</p>
+            <br />  <hr /><br />
+            {orders.map(
+                order => (
+                    <div className="container" key={order.id}>
+                        <h2>Order Id {order.id}, {order.date.toString()}</h2>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Product Name</th>
+                                    <th>Price</th>
+                                    <th>Quantity</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {order.products.map(product => (
+                                    <tr key={`${order.id}-${product.id}`}>
+                                        <td>{product.productName}</td>
+                                        <td>{product.Price}</td>
+                                        <td>{product.quantity}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <br></br><br></br>
+                    </div>
                 )
-            }
-
+            )}
         </div>
     );
 }
